7931855cbe7a0926d2255af7efca7b7ed07ed09f
aajiboye <abrahamajiboye106@gmail.com>
2022-04-25 14:23:02 +0100

    removed unused modules

--- libs/publisher/src/infrastructure/queue/queue.service.ts
+++ libs/publisher/src/infrastructure/queue/queue.service.ts
@@ -2,16 +2,12 @@ import { InjectQueue, Process, Processor } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { QUEUECONFIG } from './constants';
 import { Queue, Job } from 'bull';
-import { HttpService } from '@nestjs/axios';
 import axios from 'axios';
 
 @Injectable()
 @Processor(QUEUECONFIG.NAME)
 export class QueueService {
-  constructor(
-    @InjectQueue(QUEUECONFIG.NAME) private publisherQueue: Queue,
-    private httpService: HttpService,
-  ) {}
+  constructor(@InjectQueue(QUEUECONFIG.NAME) private publisherQueue: Queue) {}
 
   async add(payload: object) {
     await this.publisherQueue.add(payload, { removeOnComplete: true });
